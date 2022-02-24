869c078c94fb2b27952d382a31fbe7de81c73c19
godwin-pk <80232410+godwin-pk@users.noreply.github.com>
2022-02-24 21:46:52 +0530

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,6 +1,6 @@
 # Copyright Amazon.com, Inc. or its affiliates. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
-#
+# test
 resource "aws_iam_role" "aft_states" {
   name               = "aft-account-provisioning-customizations-role"
   assume_role_policy = templatefile("${path.module}/iam/trust-policies/states.tpl", { none = "none" })
